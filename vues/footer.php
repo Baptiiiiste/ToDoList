95ac203b2c6f9832a46a05614f008e6953d1796c
babonneau <baptiste.bonneau@etu.uca.fr>
2022-11-25 10:26:28 +0100

    decomposition en vues

--- vues/footer.php
+++ vues/footer.php
@@ -1,17 +1,17 @@
 
-        <footer class="container">
-            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <p class="col-md-4 mb-0 text-muted">&copy; 2022 TUDUM, Inc</p>
+<footer class="">
+    <footer class="d-flex flex-wrap align-items-center py-3 my-4 border-top">
+        <p class="col-md-4 mb-0 text-muted">&copy; 2022 TUDUM, Inc</p>
 
-                <a href="/"
-                    class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-                    <img src="img/logo.png" class="bi me-2" width="40" height="32">
-                </a>
+        <a href="/"
+            class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+            <img src="img/logo.png" class="bi me-2" width="40" height="32">
+        </a>
 
-                <ul class="nav col-md-4 justify-content-end">
-                    <li class="nav-item"><a href="/" class="nav-link px-2 text-muted">Home</a></li>
-                    <li class="nav-item"><a href="/public.php" class="nav-link px-2 text-muted">Public</a></li>
-                    <li class="nav-item"><a href="/private.php" class="nav-link px-2 text-muted">Private</a></li>
-                </ul>
-            </footer>
-        </footer>
+        <ul class="nav col-md-4 justify-content-end">
+            <li class="nav-item"><a href="" class="nav-link px-2 text-muted">Home</a></li>
+            <li class="nav-item"><a href="public.php" class="nav-link px-2 text-muted">Public</a></li>
+            <li class="nav-item"><a href="private.php" class="nav-link px-2 text-muted">Private</a></li>
+        </ul>
+    </footer>
+</footer>
---
+++ vues/task.php
@@ -0,0 +1,3 @@
+<li class=""> <!-- class="completed" si tache validée && checked=""--> 
+    <div class="form-check"> <label class="form-check-label"> <input class="checkbox" type="checkbox"> For what reason would it be advisable. <i class="input-helper"></i></label> </div> <i class="remove mdi mdi-close-circle-outline"></i>
+</li>
