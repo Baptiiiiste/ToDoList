bf94da995ddf9e9fc328b851745f86005b590cec
Loris PERRET <Loris.PERRET@etu.uca.fr>
2022-12-05 15:31:25 +0100

    create function in modele

--- model/ModelTodoList.php
+++ model/ModelTodoList.php
@@ -19,4 +19,29 @@ class ModelTodoList
     function getConnectedUser(){
         return -1;
     }
+
+    function addPublicTDL(string $name, int $owner){
+        $gateway = new ListTaskGateway(new Connection("mysql:host=localhost;dbname=todolist", "root", "loris"));
+        $gateway->insert($name, $owner);
+    }
+
+    function addPrivateTDL(string $name, int $owner, bool $visibility){
+        $gateway = new ListTaskGateway(new Connection("mysql:host=localhost;dbname=todolist", "root", "loris"));
+        $gateway->insert($name, $owner, $visibility);
+    }
+
+    function addTask(string $name, string $description, int $listTask){
+        $gateway = new TaskGateway(new Connection("mysql:host=localhost;dbname=todolist", "root", "loris"));
+        $gateway->insert($name, $description, $listTask);
+    }
+
+    function deleteTDL(int $id){
+        $gateway = new ListTaskGateway(new Connection("mysql:host=localhost;dbname=todolist", "root", "loris"));
+        $gateway->delete($id);
+    }
+
+    function deleteTask(int $id){
+        $gateway = new TaskGateway(new Connection("mysql:host=localhost;dbname=todolist", "root", "loris"));
+        $gateway->delete($id);
+    }
 }
