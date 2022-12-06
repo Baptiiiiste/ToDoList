c9f8a7d2f38fa4aff0d350341691decfe315c692
Baptiiiiste <baptiste.Tab4@gmail.com>
2022-12-06 22:35:59 +0100

    Connection (mdl & gw)

--- gateWay/UserGateway.php
+++ gateWay/UserGateway.php
@@ -3,4 +3,22 @@
 class UserGateway
 {
 
+    private $con;
+
+    public function __construct(Connection $con) {
+        $this->con = $con;
+    }
+    public function insert(string $pseudo, string $email, string $password) {
+        $query = 'INSERT INTO User (pseudo, email, password) VALUES (:pseudo, :email, :password)';
+        $this->con->executeQuery($query, array(':pseudo' => array($pseudo, PDO::PARAM_STR), ':email' => array($email, PDO::PARAM_STR), ':password' => array($password,  PDO::PARAM_STR)));
+    }
+
+    public function getPassword(string $login){
+        $login = Validation::val_string($login);
+        $query = "SELECT password FROM User WHERE login=:login";
+        $this->con->executeQuery($query, array(":login" => array($login, PDO::PARAM_STR)));
+        $res = $this->con->getResults();
+        return $res;
+    }
+
 }
--- model/ModelUser.php
+++ model/ModelUser.php
@@ -1,13 +1,28 @@
 <?php
 
-class ModelUser
-{
+class ModelUser{
+
     public function isUser(){
         if(isset($_SESSION['login'])){
             $login = Validation::val_string($_SESSION['login']);
             return new User($login);
         } else {
             return NULL;
+        }
+    }
+
+    public function connection(string $log, string $password){
+        global $base, $login, $mdp;
+        $userGateway = new UserGateway(new Connection($base, $login, $mdp));
+
+        $log = Validation::val_string($login);
+        $password = Validation::val_string($password);
+
+        $hashedPassword = $userGateway->getPassword($password);
+
+        if(password_verify($password, $hashedPassword)){
+            $_SESSION['role'] = 'user';
+            $_SESSION['login'] = $log;
         }
     }
 }
