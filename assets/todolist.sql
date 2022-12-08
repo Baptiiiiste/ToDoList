e2a3b002f36c93dfa64e02010897efe377244d38
loperret3 <loris.perret@etu.uca.fr>
2022-12-08 12:35:08 +0100

    modif sql

--- assets/todolist.sql
+++ assets/todolist.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 08 déc. 2022 à 11:10
+-- Généré le : jeu. 08 déc. 2022 à 11:32
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -31,8 +31,9 @@ DROP TABLE IF EXISTS `listtask`;
 CREATE TABLE IF NOT EXISTS `listtask` (
   `name` varchar(50) COLLATE utf8_bin NOT NULL,
   `visibility` tinyint(1) NOT NULL DEFAULT '0',
-  `owner` int(11) DEFAULT NULL,
-  PRIMARY KEY (`name`)
+  `owner` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`name`,`visibility`),
+  KEY `owner` (`owner`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -40,8 +41,7 @@ CREATE TABLE IF NOT EXISTS `listtask` (
 --
 
 INSERT INTO `listtask` (`name`, `visibility`, `owner`) VALUES
-('test', 1, NULL),
-('test2', 1, NULL);
+('test', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -51,12 +51,21 @@ INSERT INTO `listtask` (`name`, `visibility`, `owner`) VALUES
 
 DROP TABLE IF EXISTS `task`;
 CREATE TABLE IF NOT EXISTS `task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8_bin NOT NULL,
   `description` varchar(255) COLLATE utf8_bin NOT NULL,
   `done` tinyint(1) NOT NULL DEFAULT '0',
-  `listTask` int(11) NOT NULL,
-  PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `listTask` varchar(50) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `listTask` (`listTask`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `task`
+--
+
+INSERT INTO `task` (`id`, `name`, `description`, `done`, `listTask`) VALUES
+(1, 'dd', 'dd', 0, 'test');
 
 -- --------------------------------------------------------
 
@@ -66,10 +75,26 @@ CREATE TABLE IF NOT EXISTS `task` (
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-  `pseudo` varchar(50) COLLATE utf8_bin NOT NULL,
+  `login` varchar(50) COLLATE utf8_bin NOT NULL,
   `password` varchar(100) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`pseudo`)
+  PRIMARY KEY (`login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `listtask`
+--
+ALTER TABLE `listtask`
+  ADD CONSTRAINT `owner` FOREIGN KEY (`owner`) REFERENCES `user` (`login`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `task`
+--
+ALTER TABLE `task`
+  ADD CONSTRAINT `listTask` FOREIGN KEY (`listTask`) REFERENCES `listtask` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- gateWay/UserGateway.php
+++ gateWay/UserGateway.php
@@ -8,9 +8,9 @@ class UserGateway
     public function __construct(Connection $con) {
         $this->con = $con;
     }
-    public function insert(string $pseudo, string $email, string $password) {
-        $query = 'INSERT INTO User (pseudo, email, password) VALUES (:pseudo, :email, :password)';
-        $this->con->executeQuery($query, array(':pseudo' => array($pseudo, PDO::PARAM_STR), ':email' => array($email, PDO::PARAM_STR), ':password' => array($password,  PDO::PARAM_STR)));
+    public function insert(string $login, string $password) {
+        $query = 'INSERT INTO User (login, password) VALUES (:login, :email, :password)';
+        $this->con->executeQuery($query, array(':login' => array($login, PDO::PARAM_STR), ':password' => array($password,  PDO::PARAM_STR)));
     }
 
     public function getPassword(string $login){
